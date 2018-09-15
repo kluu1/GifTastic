626c4da03c7d9cf38d0eadd414ad9cc68d03aa92
kle9ej <kevin_luu@homedepot.com>
2018-09-15 14:53:06 -0400

    initial commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,67 +1,67 @@
 $( document ).ready(function () {
     
-    // Declare global variables
-
     // Get DOM elements
-    // var $moviesContainer = $('#movies-container');
-    // var $addMovie = $('#movie-input');
-    // var $addMovieBtn = $('#add-movie-btn');
-    // var $images = [$('#img-01'), $('#img-02'), $('#img-03'), $('#img-04'), $('#img-05'), $('#img-06'), $('#img-07'), $('#img-08'), $('#img-09')];
-
+    var $gifContainer = $('#gif-container');
+    var $addGif = $('#gif-input');
+    var $addGifBtn = $('#add-gif-btn');
+    var $thumbnail = $('.thumbnail');
     var $imgs = $('img');
 
-    var imgsWithIds = $imgs.filter(function (index, element) {
+    var $imgsWithIds = $imgs.filter(function (index, element) {
         return $(element).attr('id');
     });
 
-    console.log(imgsWithIds);
+    var topic = ['batman', 'spiderman', 'jumanji']
 
-    console.log(imgsWithIds[0]);
+    function startApp() {
+        $addGifBtn.on('click', addGif);
+        $gifContainer.on('click', 'button', displayGifs);
+        renderBtn();
+        $thumbnail.hide();
+    }
 
-    console.log(imgsWithIds[0].attr('src', 'http://testurl.com'))
+    function addGif() {
+        if (topic.indexOf($addGif.val()) === -1) {
+            topic.push($addGif.val());
+            var newGif = $('<button>');
+            newGif.addClass('btn btn-primary');
+            newGif.text($addGif.val());
+            $gifContainer.append(newGif);
+        } 
+    }
 
+    function renderBtn() {
+        for (i = 0; i < topic.length; i++) {
+            newBtn = $('<button>');
+            newBtn.addClass('btn btn-primary');
+            newBtn.text(topic[i]);
+            $gifContainer.append(newBtn);
+        }  
+    }
+
+    function displayGifs() {
+        $thumbnail.show();
+        var title = $(this).text().split(' ').join('+');
+        var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=D41gIqTe2XuLnlhr8V93REZDOSxjwvCx&q=' + title + '&limit=9&offset=0&rating=G&lang=en';
     
-
-    // divsWithImg[0].attr('src', 'test_rul');
-
-    // function startApp() {
-    //     $addMovieBtn.on('click', addMovie);
-    //     $moviesContainer.on('click', 'button', displayGifs);
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function(res) {
+            for (i = 0; i < 9; i++) {
+                var imgUrl = res.data[i].images.downsized.url;
+                $($imgsWithIds[i]).attr('src', imgUrl);
+            }
+        }).catch(function(err) {
+            console.log(err);
+        });
         
-    // }
+    }
 
-    // function addMovie() {
+    startApp();
 
-    //     if ($addMovie.val() != "") {
-    //         var newMovie = $('<button>');
-    //         newMovie.addClass('btn btn-primary');
-    //         newMovie.text($addMovie.val());
-    //         $moviesContainer.append(newMovie);
-    //         console.log('clicked');
-    //     } 
-    // }
-
-    // function displayGifs() {
-    //     var title = $(this).text().split(' ').join('+');
-    //     var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=D41gIqTe2XuLnlhr8V93REZDOSxjwvCx&q=' + title + '&limit=9&offset=0&rating=G&lang=en';
-    
-    //     $.ajax({
-    //         url: queryURL,
-    //         method: 'GET'
-    //     }).then(function(response) {
-
-    //         for (i = 0; i < 9; i++) {
-    //             var imgUrl = response.data[i].images.downsized.url;
-    //             // $images[i].attr('src', imgUrl);
-
-    //             imgs[0] = imgUrl;
-
-    //         }
-    //     });
-        
-    // }
-
-    // startApp();
+    console.log($imgsWithIds);
+    console.log($($imgsWithIds[10]))
     
 
 });
