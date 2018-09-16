bba5d00d8e2003ccbf24ee7387cdb7285a45626c
kle9ej <kevin_luu@homedepot.com>
2018-09-15 20:52:49 -0400

    finished animation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,6 +16,7 @@ $( document ).ready(function () {
     function startApp() {
         $addGifBtn.on('click', addGif);
         $gifContainer.on('click', 'button', displayGifs);
+        $imgsWithIds.on('click', animateGifs);
         renderBtn();
         $thumbnail.hide();
     }
@@ -25,6 +26,7 @@ $( document ).ready(function () {
             topic.push($addGif.val());
             var newGif = $('<button>');
             newGif.addClass('btn btn-primary');
+            newGif.
             newGif.text($addGif.val());
             $gifContainer.append(newGif);
         } 
@@ -42,26 +44,40 @@ $( document ).ready(function () {
     function displayGifs() {
         $thumbnail.show();
         var title = $(this).text().split(' ').join('+');
-        var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=D41gIqTe2XuLnlhr8V93REZDOSxjwvCx&q=' + title + '&limit=9&offset=0&rating=G&lang=en';
+        var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=D41gIqTe2XuLnlhr8V93REZDOSxjwvCx&q=' + title + '&limit=10&offset=0&rating=G&lang=en';
     
         $.ajax({
             url: queryURL,
             method: 'GET'
         }).then(function(res) {
-            for (i = 0; i < 9; i++) {
-                var imgUrl = res.data[i].images.downsized.url;
-                $($imgsWithIds[i]).attr('src', imgUrl);
+            for (i = 0; i < 10; i++) {
+                var still = res.data[i].images.downsized_still.url;
+                var animate = res.data[i].images.downsized.url;
+                var img = $($imgsWithIds[i]);
+                img.attr('src', still);
+                img.attr('data-still', still);
+                img.attr('data-state', 'still');
+                img.attr('data-animate', animate);
             }
         }).catch(function(err) {
             console.log(err);
-        });
-        
+        });   
+    }
+
+    function animateGifs() {
+        var state = $(this).attr('data-state');
+        var thisImg = $(this);
+
+        if (state === 'still') {
+            thisImg.attr('src', thisImg.attr('data-animate'));
+            thisImg.attr('data-state', 'animate');
+          } else if (state === 'animate') {
+            thisImg.attr('src', thisImg.attr('data-still'));
+            thisImg.attr('data-state', 'still');
+        }
     }
 
     startApp();
-
-    console.log($imgsWithIds);
-    console.log($($imgsWithIds[10]))
     
 
 });
